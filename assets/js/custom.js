c4f344898079489e5b302e4d56160d0e8beb7966
unknown <eguibeguic@gmail.com>
2024-12-05 21:19:15 -0300

    move google tag

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -98,3 +98,81 @@ document.addEventListener('DOMContentLoaded', function () {
 
     observer4.observe(videos[4]);
 });
+
+
+function snow_fall() {
+
+    const canvas = document.getElementById('snow_fall');
+    const ctx = canvas.getContext('2d');
+  
+    // Set the canvas size
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  
+  // Set Propoties
+  const numDots = 125;
+  const direction = 0.5;
+  const speed = 1.95;
+  const size = 3.5;
+  const dotColor = 'rgba(210,245,255,0.33)';
+  
+    // Set the initial dot positions
+    const dots = [];
+    for (let i = 0; i < numDots; i++) {
+      dots.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+      });
+    }
+    const rand = []
+    for (i = 0; i < numDots; i++) {
+      var random_num = Math.random() + 0.2;
+      rand.push(random_num)
+    }
+  
+    function drawDots() {
+      
+      // Clear the canvas
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+      // Draw
+      for (let i = 0; i < numDots; i++) {
+        ctx.beginPath();
+        ctx.fillStyle = dotColor;
+        ctx.arc(dots[i].x, dots[i].y, (rand[i] ** size) + (size / 1.5), 0, 2 * Math.PI);
+        ctx.fill();
+      }
+    }
+  
+        function updateDots() {
+        // Update the dot positions
+          for (let i = 0; i < numDots; i++) {
+            dots[i].y += (speed / 2) + ((Math.sin(rand[i]) * 1.25) * speed);
+            dots[i].x += (Math.cos((dots[i].y * (Math.sqrt(speed) / speed / 10))) * ((Math.sqrt(Math.sqrt(speed) ** 1.3) - 0.15)) / 4) + direction;
+            
+            if (dots[i].y > canvas.height) {
+              // Reset the dot position and speed
+              dots[i].y = 0;
+            } else if (dots[i].x > canvas.width) {
+              dots[i].x = 0;
+            }  else if (dots[i].x < 0) {
+              dots[i].x = canvas.width;
+            }
+          }
+        }
+  
+        // Animate the dots
+        function animate() {
+          // Update the dot positions
+          updateDots();
+  
+          // Draw the dots
+          drawDots();
+          // Request the next animation frame
+          requestAnimationFrame(animate);
+        }
+  
+        // Start the animation
+        animate();
+  }
+  snow_fall();
